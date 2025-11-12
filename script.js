3f41f0b22f8d42411e61e261203c2a770fcbec87
Ariel Mon <ben10ariel@gmail.com>
2025-11-12 11:15:49 -0500

    fix the script running before html ellemnts

--- script.js
+++ script.js
@@ -1,50 +1,51 @@
-const wordsArray = [
-  "apple",
-  "table",
-  "world",
-  "grape",
-  "light",
-  "audio",
-  "music",
-  "dream",
-  "water",
-  "stone",
-  "smile",
-  "cloud"
-]
-
-//12 words 
-
-function selectRandomWord(wordsArray) {
+document.addEventListener('DOMContentLoaded', () => {
+    const wordsArray = [
+      "apple",
+      "table",
+      "world",
+      "grape",
+      "light",
+      "audio",
+      "music",
+      "dream",
+      "water",
+      "stone",
+      "smile",
+      "cloud"
+    ]
     
-    //check if an array is empty inn case for errors
-    if (!Array.isArray(wordsArray) || wordsArray.length === 0) {
-        return undefined;
+    //12 words 
+    
+    function selectRandomWord(wordsArray) {
+        
+        //check if an array is empty inn case for errors
+        if (!Array.isArray(wordsArray) || wordsArray.length === 0) {
+            return undefined;
+        }
+    
+        //generate a random index between 0 and the array list lenght at first 12
+        const randomIndex = Math.floor(Math.random() * wordsArray.length);
+        return wordsArray[randomIndex];
     }
-
-    //generate a random index between 0 and the array list lenght at first 12
-    const randomIndex = Math.floor(Math.random() * wordsArray.length);
-    return wordsArray[randomIndex];
-}
-
-
-//call the function with the array
-const randomWord = selectRandomWord(wordsArray);
-
-// show the result of the choosing to the console
-console.log(randomWord);
-
-
-//on screen keyboard listerners
-// get all keys from keyboard
-const keyboardKeys = document.querySelectorAll(".key");
-
-//Function to handle letter input being pressed
-function handleLetterInput(letter) {
-    console.log(`letter pressed: ${letter}`);
-}
-
-// enter key
+    
+    
+    //call the function with the array
+    const randomWord = selectRandomWord(wordsArray);
+    
+    // show the result of the choosing to the console
+    console.log(randomWord);
+    
+    
+    //on screen keyboard listerners
+    // get all keys from keyboard
+    const keyboardKeys = document.querySelectorAll(".key");
+    
+    //Function to handle letter input being pressed
+    function handleLetterInput(letter) {
+        console.log(`letter pressed: ${letter}`);
+    }
+    
+    // enter key
     function handleEnter() {
         console.log('enter pressed');
     }
@@ -54,19 +55,20 @@ function handleDelete() {
     console.log('delete pressed');
 } //ADD THE LOGIC LATER ON bruh
 
-
-
-// click listner for each key
-keyboardKeys.forEach(key => {
-    key.addEventListener('click', () => {
-        const keyValue = key.dataset.key; // get value from each data attribute in html
-        if (keyValue === 'ENTER') {
-            handleEnter();
-        } else if (keyValue === 'DEL') {
-            handleDelete();
-        } else {
-            handleLetterInput(keyValue);
-        } 
-            
+    
+    
+    // click listner for each key
+    keyboardKeys.forEach(key => {
+        key.addEventListener('click', () => {
+            const keyValue = key.dataset.key; // get value from each data attribute in html
+            if (keyValue === 'ENTER') {
+                handleEnter();
+            } else if (keyValue === 'DEL') {
+                handleDelete();
+            } else {
+                handleLetterInput(keyValue);
+            } 
+                
+            });
         });
-    });
+});
